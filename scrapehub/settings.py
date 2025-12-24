43f804ea750a446bcc081712b1b2798b75b2de72
Bashir Ahmed <bashir@islahwebservice.com>
2025-12-24 13:08:52 +0600

    job resume

--- scrapehub/settings.py
+++ scrapehub/settings.py
@@ -186,5 +186,4 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 CELERY_TASK_TRACK_STARTED = True
-CELERY_TASK_TIME_LIMIT = 30 * 60  # 30 minutes
-
+CELERY_TASK_TIME_LIMIT = 43200  # 12 hours (increased from 30 min for large scraping jobs)
